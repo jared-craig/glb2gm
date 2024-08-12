1d78b4bfed067583b559b07a747f40a7e8b4e431
jared-craig <54082674+jared-craig@users.noreply.github.com>
2024-08-12 10:39:00 -0500

    logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import { Box, Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 
 export default function Home() {
   return (
-    <Box>
-      <Typography variant='h6'>HOME</Typography>
-    </Box>
+    <Stack>
+      <Typography variant='h6'>Welcome to GLB2GM!</Typography>
+      <Typography variant='body1'>App is currently in development</Typography>
+      <Typography variant='body1'>Use the menu to view stats</Typography>
+    </Stack>
   );
 }
