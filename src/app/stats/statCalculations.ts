e14600809b6b842a7498338a7a41d0eb360daeab
jared-craig <54082674+jared-craig@users.noreply.github.com>
2024-08-14 16:13:30 -0500

    Update statCalculations.ts (#44)
    * Update statCalculations.ts
    
    * Update statCalculations.ts

--- src/app/stats/statCalculations.ts
+++ src/app/stats/statCalculations.ts
@@ -2,11 +2,11 @@ import { GridRowModel } from '@mui/x-data-grid-pro';
 import { PlayerPassingData } from './passing/playerPassingData';
 
 export const getPassingGmRating = (row: GridRowModel): number => {
-  return Math.round(0.2 * +row.yards + 5.0 * +row.touchdowns - 5.0 * +row.interceptions - +row.sacks);
+  return Math.round(0.2 * +row.yards + 2.0 * +row.touchdowns - 2.0 * +row.interceptions + 100.0 * +row.yards_per_attempt - +row.sacks);
 };
 
 export const getReceivingGmRating = (row: GridRowModel): number => {
-  return Math.round(0.2 * +row.yards + 5.0 * +row.touchdowns - 100.0 - +row.drops - +row.fumbles_lost);
+  return Math.round(0.2 * +row.yards + 2.0 * +row.touchdowns - 10.0 * +row.drops - +row.fumbles_lost);
 };
 
 export const getRushingGmRating = (row: GridRowModel): number => {
