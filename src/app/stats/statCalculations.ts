8965b34ee643fbb98396f61589c678a8355648fa
jared-craig <54082674+jared-craig@users.noreply.github.com>
2024-08-20 13:50:49 -0500

    reduce int yards (#67)

--- src/app/stats/statCalculations.ts
+++ src/app/stats/statCalculations.ts
@@ -10,7 +10,7 @@ export const getDefensiveGmRating = (x: any): number => {
       1.0 * +x.fumble_recoveries +
       1.0 * +x.passes_defended +
       1.0 * +x.passes_knocked_loose +
-      0.5 * +x.interception_yards
+      0.25 * +x.interception_yards
   );
 };
 
