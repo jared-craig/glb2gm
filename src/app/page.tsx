1bc7db3a2d43dee9c0607bf52a80f4706460f0d6
jared-craig <54082674+jared-craig@users.noreply.github.com>
2024-09-06 07:27:31 -0500

    package updates, front page updates (#93)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,29 @@
 'use client';
 
-import { Stack, Typography } from '@mui/material';
+import { List, ListItem, ListItemText, Stack, Typography } from '@mui/material';
 
 export default function Home() {
   return (
     <Stack spacing={2}>
-      <Stack spacing={1}>
+      <Stack spacing={3}>
         <Typography variant='h6'>Welcome to GLB2GM!</Typography>
-        <Typography variant='body1'>App is currently in development</Typography>
-        <Typography variant='body1'>Use the menu to view stats</Typography>
+        <Typography variant='body1'>
+          App is currently in <i>development</i>
+        </Typography>
+        <Stack spacing={-1}>
+          <Typography variant='body1'>Current Features:</Typography>
+          <List>
+            <ListItem disablePadding>
+              <ListItemText primary='All Stars' secondary='1st and 2nd team All Stars according to the GM Rating system.'></ListItemText>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemText primary='Player Builder' secondary="Select a position, template, and what's important to you."></ListItemText>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemText primary='Player Statistics' secondary="By tier and category. Filter and sort to your heart's content."></ListItemText>
+            </ListItem>
+          </List>
+        </Stack>
         <Typography variant='caption'>
           Developed by <strong>MadKingCraig</strong>
         </Typography>
