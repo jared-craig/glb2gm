3fb86e9057fb6895998d8edf1d547941e21fc5e1
jared-craig <54082674+jared-craig@users.noreply.github.com>
2024-09-23 13:46:31 -0500

    attempt to stop translation prompt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' translate='no'>
       <head>
         <link rel='icon' href='/favicon.ico' />
       </head>
