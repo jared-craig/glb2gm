615b43bd95a6672caa2696e08af08aba4c9eafd1
jared-craig <54082674+jared-craig@users.noreply.github.com>
2024-10-16 21:25:19 -0500

    Update route.ts (#204)
    * Update route.ts
    
    * Update route.ts

--- src/app/api/player-optimizer/route.ts
+++ src/app/api/player-optimizer/route.ts
@@ -99,7 +99,7 @@ export async function POST(request: NextRequest) {
   const { position, attCombos, traitCombos, heightWeightCombos, data, filteredData, skillMins }: PostRequestProps = reqData;
 
   let best: any = { sp: Number.NEGATIVE_INFINITY };
-  let bestFailing: any = { sp: Number.NEGATIVE_INFINITY };
+  let bestFailing: any = { sp: Number.NEGATIVE_INFINITY, cbr: Number.NEGATIVE_INFINITY };
 
   for (const hwCombo of heightWeightCombos) {
     for (const attCombo of attCombos) {
@@ -167,7 +167,7 @@ export async function POST(request: NextRequest) {
 
         if (skillPoints > best.sp && canAchieveBuild)
           best = { ...hwCombo, ...attCombo, ...traitCombo, sp: skillPoints, cbr: capBoosts, cbs: capBoostsSpent, build: suggestedBuild };
-        if (skillPoints > bestFailing.sp && capBoosts > -3 && !canAchieveBuild)
+        if (skillPoints > bestFailing.sp && capBoosts >= bestFailing.cbr && !canAchieveBuild)
           bestFailing = { ...hwCombo, ...attCombo, ...traitCombo, sp: skillPoints, cbr: capBoosts, cbs: capBoostsSpent, build: suggestedBuild };
       }
     }
