6eb6687b9b2f6a5dbd356cec8bb689d14e7e4fca
jared-craig <54082674+jared-craig@users.noreply.github.com>
2024-10-29 12:48:43 -0500

    player templates (#241)
    salaries,
    qb pocket

--- src/app/player-templates/templates.ts
+++ src/app/player-templates/templates.ts
@@ -17,6 +17,45 @@ export interface Template {
 
 export const getTemplates = (position: string): Template[] => {
   const templates: Template[] = [
+    // QB
+    {
+      position: 'QB',
+      templateName: 'Pocket',
+      height: 78,
+      weight: 253,
+      strength: 1,
+      speed: 1,
+      agility: 10,
+      stamina: 6,
+      awareness: 7,
+      confidence: 10,
+      trait1: 'superstar_glam',
+      trait2: 'qb_pocket_passer',
+      trait3: 'early_bloomer',
+      skills: {
+        pass_technique: 100.0,
+        pass_accuracy: 90.0,
+        pass_power: 60.0,
+        pass_consistency: 100.0,
+        pass_grip: 25.0,
+        pass_evasiveness: 65.0,
+        pass_carry_power: 0.0,
+        pass_awareness: 40.0,
+        carry_grip: 0.0,
+        carry_power: 0.0,
+        carry_elusiveness: 0.0,
+        carry_awareness: 0.0,
+        balance: 50.0,
+        footwork: 30.0,
+        quickness: 0.0,
+        sprinting: 0.0,
+        conditioning: 50.0,
+        toughness: 30.0,
+        heart: 50.0,
+        intimidation: 0.0,
+        leadership: 80.0,
+      },
+    },
     // HB
     {
       position: 'HB',
