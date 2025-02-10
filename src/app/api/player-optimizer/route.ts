886fad101bbbd9ea80102178bcb817550562a4c5
jared-craig <54082674+jared-craig@users.noreply.github.com>
2025-02-10 11:00:00 -0600

    better isBuild1Better for optimizer

--- src/app/api/player-optimizer/route.ts
+++ src/app/api/player-optimizer/route.ts
@@ -128,8 +128,8 @@ const isBuild1Better = (build1: any, build2: any): boolean => {
   if (build1.sp <= build2.sp && build1.cbr <= build2.cbr) return false;
   if (build1.sp > build2.sp && build1.cbr > build2.cbr) return true;
 
-  const build1Value = build1.sp - Math.abs(build1.cbr) * 2500;
-  const build2Value = build2.sp - Math.abs(build2.cbr) * 2500;
+  const build1Value = build1.sp - (build1.cbr >= 0 ? 0 : Math.abs(build1.cbr) * 5000);
+  const build2Value = build2.sp - (build2.cbr >= 0 ? 0 : Math.abs(build2.cbr) * 5000);
   return build1Value > build2Value;
 };
 
