d1852e47aa2ce3c10fd159e1b366530b2554d02c
jared-craig <54082674+jared-craig@users.noreply.github.com>
2025-03-04 08:01:26 -0600

    lint and npm updates

--- src/app/api/teams/route.ts
+++ src/app/api/teams/route.ts
@@ -1,10 +1,8 @@
 import { PrismaClient } from '@prisma/client';
-import { NextRequest } from 'next/server';
 
 const prisma = new PrismaClient();
 
-export async function GET(request: NextRequest) {
-  const params = request.nextUrl.searchParams;
+export async function GET() {
   const allTeamData = await prisma.ladder.findMany({
     where: {
       season: process.env.CURRENT_SEASON ? +process.env.CURRENT_SEASON : undefined,
